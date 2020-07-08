04a9b0d24fdea42d0bdd3defe3508e31b52ca39b
Jonathan <jonathancamiling@gmail.com>
2020-07-07 23:01:09 -0700

    rendering box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Box from '../src/Components/Box';
+import BoxList from '../src/Components/BoxList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box height={10} width={20} color='purple' />
+      <Box height={20} width={15} color='gold' />
+      <BoxList />
     </div>
   );
 }
