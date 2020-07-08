2932ae8b2c336b3920500d4dfa35a93d773c6486
Jonathan <jonathancamiling@gmail.com>
2020-07-07 23:24:14 -0700

    updated index file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Register(props) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("authenticated!", { email, password });
+  }
+
+  return (
+    <div className="container">
+      <h1>Register Here</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={e => setEmail(e.target.value)}
+          name="email"
+          value={email}
+          autoComplete="off"
+          placeholder="Your email address"
+        />
+        <input
+          onChange={e => setPassword(e.target.value)}
+          name="password"
+          type="password"
+          value={password}
+          placeholder="Choose a safe password"
+        />
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Register />, rootElement);
